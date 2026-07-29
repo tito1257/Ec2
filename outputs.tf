6cb620d51ad80d317ab4ad71596e727c17234db7
tito1257 <150913966+tito1257@users.noreply.github.com>
2026-07-29 11:00:25 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output = "ec2_instance_id"{
+output  "ec2_instance_id"{
     value = aws_instance.this.id
 }
-output = "ec2_instance_privateip"{
+output "ec2_instance_privateip"{
     value = aws_instance.this.private_ip
 }
-output = "sg_id"{
+output  "sg_id"{
     value = aws_security_group.sg.id 
 }
